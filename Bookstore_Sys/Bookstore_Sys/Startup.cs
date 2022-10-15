2f39d0f5b4563c9671c266c1125d00213260d8f7
danny1227 <danielcoroy1200@gmail.com>
2022-10-15 01:42:57 -0600

    CU-V1: Create firts version for project bookstore

---
+++ Bookstore_Sys/Bookstore_Sys/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bookstore_Sys.Startup))]
+namespace Bookstore_Sys
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
